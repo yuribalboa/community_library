053f5df73e674b2b2e45afe9d71001e2b20eef1f
YuriTropeia <yurineudinetropeia@gmail.com>
2024-08-27 22:59:55 -0300

    bcrypt and nodemon

--- src/service/user.services.js
+++ src/service/user.services.js
@@ -1,11 +1,19 @@
 import userRepository from '../repositories/user.repositories.js';
+import bcrypt from 'bcrypt';
 
 async function  createUserService(newUser){    
     const foundUser = await userRepository.findUserByEmailRepository(newUser.email);
     
     if (foundUser) throw new Error("User already exists!");     
     
-    const user = await userRepository.createUserRepository(newUser);      
+    const passHash = await bcrypt.hash(newUser.password, 10);
+    
+    const user = await userRepository.createUserRepository({
+        ...newUser, 
+        password: passHash,
+    });
+    
+    if(!user) throw new Error("Error creating User");
     
     return user;    
 };
